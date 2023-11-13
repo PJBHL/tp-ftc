a3d981f648f93686ba3f279e22b0127eee627fae
João Pedro <joaoplp22@gmail.com>
2023-11-13 18:34:59 -0300

    feat: leitura de gramatica

--- cyk_algorithm/src/Componentes/Gramatica.java
+++ cyk_algorithm/src/Componentes/Gramatica.java
@@ -1,54 +1,106 @@
 package Componentes;
-
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.File;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-// Exemplo de leitura gramatica
-// [0] -> [0][1][2][3]
+import java.util.HashMap;
 
 public class Gramatica {
-  private List<String> leituraGramatica = new ArrayList<>();
+  private Map<String, List<String>> leituraGramatica;
 
-  public Gramatica(String path) {
-    File arq = new File(path);
-    String caminhoTotal = arq.getAbsolutePath();  
+  public Gramatica(String path) throws Exception {
+    // File arq = new File(path);
+    // String caminhoTotal = arq.getAbsolutePath();  
 
-    // String[] splitado = caminhoTotal.split("/");
-    // List<String> listona = new ArrayList<>(Arrays.asList(splitado));
+    // // String[] splitado = caminhoTotal.split("/");
+    // // List<String> listona = new ArrayList<>(Arrays.asList(splitado));
     
 
-    // LOBATO: C:\Users\joaopc\Documents\tp-ftc\cyk_algorithm\src\Gramatica\g1.txt
+    // // LOBATO: C:\Users\joaopc\Documents\tp-ftc\cyk_algorithm\src\Gramatica\g1.txt
 
-    leituraGramatica = readGrammar(caminhoTotal);
-    System.out.println("LOBATO: " + caminhoTotal);
+    leituraGramatica = readGrammar(path);
+    // System.out.println("LOBATO: " + caminhoTotal);
+
+    
 
   }
 
-  public List<String> readGrammar(String path) {
 
-    List<String> grammarLines = new ArrayList<>();
-    try {
+  // Exemplo de leitura gramatica
+  // [0] -> [0][1][2][3]
+  // [1] -> [0][1][2][3]
 
-      BufferedReader br = new BufferedReader(new FileReader(path));
-      String line;
 
-      while ((line = br.readLine()) != null) {
-        grammarLines.add(line);
-      }
 
-      br.close();
 
-    } catch (IOException e) {
-      System.out.println("Erro na leitura E: " + e.getMessage());
+  // public List<List<String>> readGrammar(String path) throws Exception {
+  //   List<List<String>> gramatica = new ArrayList<>();
+  //   BufferedReader br = new BufferedReader(new FileReader(path));
+
+  //   String line;
+
+  //   while((line = br.readLine()) != null) {
+  //     line = line.trim();
+
+  //     if(!line.isEmpty()) {
+  //       String[] delimitador = line.split("->");
+  //       String naoTerminal = delimitador[0].trim();
+  //       String[] regras = delimitador[1].trim().split("\\|");
+
+  //       List<String> regrasList = new ArrayList<>();
+  //       regrasList.add(naoTerminal);
+  //       for(String regra : regras)
+  //         regrasList.add(regra.trim());
+
+  //       gramatica.add(regrasList);
+  //     }
+  //   }
+
+  //   printGramatica(gramatica);
+  //   br.close();
+  //   return gramatica;
+  // }
+
+  // public void printGramatica(List<List<String>> gramatica) {
+  //   for (int i = 0; i < gramatica.size(); i++) {
+  //       System.out.print("[" + i + "] " + gramatica.get(i).get(0) + ": ");
+  //       for (int j = 1; j < gramatica.get(i).size(); j++) {
+  //           System.out.print(gramatica.get(i).get(j));
+  //           if (j < gramatica.get(i).size() - 1) {
+  //               System.out.print(", ");
+  //           }
+  //       }
+  //       System.out.println();
+  //   }
+  // }
+
+
+  public Map<String, List<String>> readGrammar(String path) throws Exception {
+    BufferedReader br = new BufferedReader(new FileReader(path));
+    Map<String, List<String>> gramatica = new HashMap<>();
+    
+    String line;
+
+    while((line = br.readLine()) != null) {
+      String[] delimitador = line.split(" -> ");
+      String naoTerminal = delimitador[0].trim();
+      String[] regras = delimitador[1].trim().split("\\|");
+
+      List<String> regrasList = new ArrayList<>();
+      for(String regra : regras)
+        regrasList.add(regra.trim());
+
+      gramatica.put(naoTerminal, regrasList);
+      
 
     }
 
-    return grammarLines;
+    System.out.println(gramatica);
 
+    br.close();
+    return gramatica;
   }
 }
+
