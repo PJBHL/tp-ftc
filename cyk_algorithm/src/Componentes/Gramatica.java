f71e1fe2ac1e4dbe597cf6780e92fef6be561283
Vinicius Giovanini <vgiovanini@sga.pucminas.br>
2023-11-13 17:59:23 -0300

    feat: Leitura e tratamento do path da gramatica por debug

--- cyk_algorithm/src/Componentes/Gramatica.java
+++ cyk_algorithm/src/Componentes/Gramatica.java
@@ -14,20 +14,62 @@ import java.util.Arrays;
 public class Gramatica {
   private List<String> leituraGramatica = new ArrayList<>();
 
-  public Gramatica(String path) {
+  /**
+   * Este método trata o path.
+   * 
+   * @param path Caminho do arquivo de texto.
+   * @return Path tratado com o caminho total do arquivo.
+   */
+
+  private String tratarPath(String path) {
+
     File arq = new File(path);
-    String caminhoTotal = arq.getAbsolutePath();  
+    String caminhoTotal = arq.getAbsolutePath();
+    String array[] = caminhoTotal.split("\\\\");
+    List<String> list = new ArrayList<>(Arrays.asList(array));
+    var arqName = list.get(list.size() - 1);
+    var folder1 = list.get(list.size() - 2);
+    list.remove(list.size() - 1);
+    list.remove(list.size() - 1);
+    list.add("src");
+    list.add(folder1);
+    list.add(arqName);
 
-    // String[] splitado = caminhoTotal.split("/");
-    // List<String> listona = new ArrayList<>(Arrays.asList(splitado));
-    
+    String pathFinal = "";
 
-    // LOBATO: C:\Users\joaopc\Documents\tp-ftc\cyk_algorithm\src\Gramatica\g1.txt
+    for (String each : list) {
+      pathFinal += each + "/";
+    }
 
-    leituraGramatica = readGrammar(caminhoTotal);
-    System.out.println("LOBATO: " + caminhoTotal);
+    if (pathFinal != "") {
+      return pathFinal;
+    } else {
+      return "Path nao encontrado";
+    }
 
   }
+
+  /**
+   * Este método CONSTRUTOR.
+   * 
+   * @param path Caminho do arquivo de texto.
+   */
+  public Gramatica(String path) {
+
+    String pathTratado = this.tratarPath(path);
+
+    List<String> gramaticaLida = this.readGrammar(pathTratado);
+
+    System.out.println(gramaticaLida);
+
+  }
+
+  /**
+   * Este método faz a leiturado do arquivo texto.
+   * 
+   * @param path Caminho do arquivo de texto.
+   * @return Lista com todas as regras da gramatica.
+   */
 
   public List<String> readGrammar(String path) {
 
