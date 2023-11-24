bc004d60151920ef3d46b939d70dbeaef15fef5f
João Pedro <joaoplp22@gmail.com>
2023-11-24 12:19:13 -0300

    fix: forma binária finalizada.

--- cyk_algorithm/src/Componentes/CNF/ConverterNaoTerminais.java
+++ cyk_algorithm/src/Componentes/CNF/ConverterNaoTerminais.java
@@ -5,28 +5,9 @@ import java.util.*;
 import Componentes.Gramatica;
 
 public class ConverterNaoTerminais {
-
-    /**
-     * Método para verificar a quantidade de letras maiusculas em determinada regra.
-     * 
-     * @param texto - regra analisada
-     * @return - true se é diferente de 2 letras maiusculas.
-     */
-    private static boolean verificarQuantidadeMaiusculas(String texto) {
-        int contadorMaiusculas = 0;
-        for (int i = 0; i < texto.length(); i++) {
-            char caractere = texto.charAt(i);
-            if (Character.isUpperCase(caractere)) {
-                contadorMaiusculas++;
-            } else {
-                return false;
-            }
-        }
-        return contadorMaiusculas != 2;
-    }
-
     public static void converterNaoTerminais(Map<String, List<String>> mapa) {
         Map<String, String> substituicoes = new HashMap<>();
+        List<String> naoTerminais = Gramatica.pegarNaoTerminais(mapa);
 
         for (Map.Entry<String, List<String>> entry : mapa.entrySet()) {
             String key = entry.getKey();
@@ -35,7 +16,7 @@ public class ConverterNaoTerminais {
             for (int i = 0; i < values.size(); i++) {
                 String original = values.get(i);
                 if (original.length() > 2) {
-                    String substituido = substituirDuplasMaiusculas(original, substituicoes);
+                    String substituido = substituirDuplasMaiusculas(original, substituicoes, naoTerminais);
                     values.set(i, substituido);
                 }
             }
@@ -47,26 +28,80 @@ public class ConverterNaoTerminais {
         }
     }
 
-    public static String substituirDuplasMaiusculas(String original, Map<String, String> substituicoes) {
-        String resultado = original;
+    /**
+     * S -> {JAAAA | JAA | JA | a}
+     * A -> {JAA | JA | a}
+     * G -> {a}
+     * Map<String, String> = J1 -> JA
+     * Map<String, String> = A1 -> AA
+     * 
+     * J1A1A
+     * Map<String, String> A2 -> A1A
+     * A2A
+     * 
+     * S -> {M1AA | M1A | MA | a}
+     * A -> {M1A | MA | a}
+     * U -> {a}
+     * M1 -> MA
+     * 
+     * S -> {J3A | J1A | JA | a}
+     * A -> {J1A | JA | a}
+     * G -> {a}
+     * J1 -> JA
+     * J2 -> J1A
+     * J3 -> J2A
+     * 
+     */
+    public static List<String> dividirString(String input) {
+        List<String> newArray = new ArrayList<>();
 
-        for (int i = 0; i < original.length() - 1; i++) {
-            if (Character.isUpperCase(original.charAt(i)) && Character.isUpperCase(original.charAt(i + 1))) {
-                String substring = original.substring(i, i + 2);
-                String substituicao = substituicoes.get(substring);
-                if (substituicao == null) {
-                    substituicao = substituirDupla(substring);
-                    substituicoes.put(substituicao, substring);
-                }
-                resultado = resultado.replace(substring, substituicao);
-                i = original.length();
+        String regra = String.valueOf(input.charAt(0));
+
+        for (int i = 1; i < input.length(); i++) {
+            if (Character.isUpperCase(input.charAt(i))) {
+                newArray.add(regra);
+                regra = String.valueOf(input.charAt(i));
+            } else {
+                regra += String.valueOf(input.charAt(i));
             }
+        }
+
+        newArray.add(regra);
+
+        return newArray;
+    }
+
+    public static String substituirDuplasMaiusculas(String original, Map<String, String> substituicoes,
+            List<String> naoTerminais) {
+        String resultado = original;
+        boolean verificar = false;
+
+        List<String> splitResultado = dividirString(resultado);
+        int pos = 1;
+
+        for (int i = 0, contador = 1; splitResultado.size() != 2; i++, contador++) {
+            String letra = splitResultado.get(i) + splitResultado.get(i + 1);
+
+            // String substring = letra + splitResultado.get(i + 2);
+            // substring = letra
+            String substituicao = substituicoes.get(letra);
+            substituicao = substituirDupla(letra, contador);
+            if (!naoTerminais.contains(substituicao)) {
+                naoTerminais.add(substituicao);
+                substituicoes.put(substituicao, letra);
+                resultado = resultado.replace(letra, substituicao);
+            } else {
+                resultado = resultado.replace(letra, substituicao);
+            }
+            i--;
+
+            splitResultado = dividirString(resultado);
         }
 
         return resultado;
     }
 
-    public static String substituirDupla(String dupla) {
-        return dupla.substring(0, 1) + "1";
+    public static String substituirDupla(String dupla, int contador) {
+        return dupla.substring(0, 1) + String.valueOf(contador);
     }
 }
