c13bb13864746a880e959aaa7169694fefa79c27
João Pedro <joaoplp22@gmail.com>
2023-11-28 15:42:24 -0300

    fix: pegar unitários agora retorna gramática

--- cyk_algorithm/src/Componentes/CykModificado.java
+++ cyk_algorithm/src/Componentes/CykModificado.java
@@ -36,44 +36,43 @@ public class CykModificado {
         return transicoesVazias;
     }
 
-    /** Mudar, tem que ser um map - pegar o valor do naoTerminal que vai para ele também. */
-    public static Set<String> pegarUnitarios(Map<String, List<String>> glc, List<String> nullables) {
-        Map<String, List<String>> glcCopy = Gramatica.clonarGramatica(glc);
-        Set<String> unitarios = new LinkedHashSet<>();
-
-        nullables = nullable(glcCopy);
+    public static Map<String, List<String>> pegarUnitarios(Map<String, List<String>> glc, List<String> nullables) {
+        Map<String, List<String>> glcUnitarios = new LinkedHashMap<>();
 
         for (Map.Entry<String, List<String>> entry : glc.entrySet()) {
             String naoTerminal = entry.getKey();
             List<String> regras = entry.getValue();
+            List<String> adicionaveis = new ArrayList<>();
 
             for(String regra : regras) {
                 if(regra.length() >= 2) {
                     // Unitários juntos de não terminais que formam lambda.
                     if(nullables.stream().anyMatch(regra::contains)) {
                         String terminal = new String(regra).replaceAll("[A-Z]", "");
-                        unitarios.add(terminal);
+                        adicionaveis.add(terminal);
                     }
-                } else {
-                    // Unitários sozinhos.
-                    unitarios.add(regra);
+                } else if(!regra.equals("!")) {
+                    adicionaveis.add(regra);
                 }
             }
+            glcUnitarios.put(naoTerminal, new ArrayList<>(adicionaveis));
         }
 
 
-        unitarios.remove("!");
-        System.out.println("\nUnitários: " + unitarios);
-
-        return unitarios;
+        return glcUnitarios;
     }
 
     public static void main(String[] args) throws Exception {
         Gramatica glc = new Gramatica(args[0]);
         Forma2NF nf = new Forma2NF(glc);
+
         Map<String, List<String>> gramatica = Forma2NF.glc;
-        List<String> transicoesVaizas = nullable(gramatica);
+        Map<String, List<String>> glcCopy = Gramatica.clonarGramatica(gramatica);
+
+        List<String> transicoesVaizas = nullable(glcCopy);
         System.out.println("\nLista com os vazios: " + transicoesVaizas);
-        pegarUnitarios(gramatica, transicoesVaizas);
+
+        Map<String, List<String>> gramaticaUnitarios = pegarUnitarios(gramatica, transicoesVaizas);
+        Gramatica.imprimirGramatica(gramaticaUnitarios);
     }
 }
